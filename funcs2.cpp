4e78d0e4dfeb07635a1d449237aa3acfab7c3c23
Hugo Valle <hugovalle1@weber.edu>
2017-09-12 16:33:51 -0600

    Another commit

---
+++ funcs2.cpp
@@ -0,0 +1,21 @@
+// File: funcs2
+// Created by Hugo Valle on 9/12/2017.
+// Copyright (c) 2017 WSU
+//
+
+#include <iostream>
+
+using namespace std;
+// Constants 
+
+// Prototypes
+
+// Main Program
+int main()
+{
+	cout<<"Yes sir. I got it"<< endl;
+	
+	return 0;
+}
+
+// Function Definitions
